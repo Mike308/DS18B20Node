09b844391351570bfd5e978fa273341b4398ef0e
Mikołaj Werner <m.werner9205@gmail.com>
2018-05-21 21:07:56 +0200

    - added handling DS18B20 sensor and nrf24l01 module

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "RF24Network.h"
 
-void setup() {
-    // put your setup code here, to run once:
+uint64_t pipeAddress = 0xF0F0F0F0F101;
+uint64_t receivingPipeAddress = 0xF0F0F0F0E101;
+char rxBuf[128];
+
+RF24Network node(9, 10, "ds18b20Node");
+OneWire oneWire(2);
+DallasTemperature sensors(&oneWire);
+
+
+void setup()
+{
+    sensors.begin();
+    node.begin();
+    node.begin();
+    node.openWritingPipe(pipeAddress);
+    node.openReadingPipe(1, receivingPipeAddress);
+    node.startListening();
+    Serial.begin(9600);
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void loop()
+{
+    unsigned long currentMillis = millis();
+    if ((currentMillis % 3000) == 0)
+    {
+        sensors.requestTemperatures();
+        float temperature = sensors.getTempCByIndex(0);
+        String atCommand = "AT+DS18B20=";
+        String strValues = String(temperature);
+        atCommand += strValues;
+        node.sendMessage(atCommand);
+        Serial.println(atCommand);
+    } 
 }
