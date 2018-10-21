acc623f4afeed0c725ce0da1b5b4a5d1494aeae5
Mikołaj Werner <m.werner9205@gmail.com>
2018-10-21 21:30:46 +0200

    - code refactored

--- src/main.cpp
+++ src/main.cpp
@@ -4,38 +4,32 @@
 #include <DallasTemperature.h>
 #include <printf.h>
 #include "RF24Network.h"
+#include "Node.h"
 
 uint64_t pipeAddress = 0xF0F0F0F0F101;
 uint64_t receivingPipeAddress = 0xF0F0F0F0E101;
 char rxBuf[32];
+uint8_t sensorAddressArray[8];
+unsigned long  currentMillis;
 
-RF24Network node(9, 10, "ds18Node");
+RF24Network node(9, 10, "0");
 OneWire oneWire(2);
 DallasTemperature sensors(&oneWire);
+Node customNode(&sensors, &node);
 
 void setup() {
   Serial.begin(9600);
   node.begin();
+  node.setRetries(15, 15);
   node.openWritingPipe(pipeAddress);
   node.openReadingPipe(1, receivingPipeAddress);
+  node.setAutoAck(true);
   node.startListening();
   sensors.begin();
 }
 
 void loop() {
-
-
-
-  if (node.receiveMessage(rxBuf)) {
-    char *atCommand = strtok(rxBuf, "?");
-    if (strcmp(atCommand, "AT+DS18B20") == 0) {
-      sensors.requestTemperatures();
-      float temperature = sensors.getTempCByIndex(0);
-      String atCommandStr = "AT+DS18B20=";
-      String strValues = String(temperature);
-      atCommandStr += strValues;
-      node.sendMessage(atCommandStr);
-      Serial.println(atCommandStr);
-    }
-  }
+  currentMillis = millis();
+  customNode.poolSensors(currentMillis);
+  customNode.start();
 }
